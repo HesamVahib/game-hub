6341744ee79db498cc71b4af0d84a8fead627ccb
Hesam V. <hesam.vahib@gmail.com>
2025-02-09 03:26:01 +0200

    initial

---
+++ game-hub/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, ButtonGroup } from "@chakra-ui/react"
+
+
+function App() {
+  return (
+    <ButtonGroup size="sm" variant="outline">
+      <Button colorPalette="blue">Save</Button>
+      <Button>Cancel</Button>
+    </ButtonGroup>
+  )
+}
+
+export default App
